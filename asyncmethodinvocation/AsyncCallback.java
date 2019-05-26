05ad99ed9af73cb0d7cf2ca7b40ee73916d119b4
Raghaava <javvaji.raghava@gmail.com>
2019-05-26 10:08:51 -0700

    change package

--- asyncmethodinvocation/AsyncCallback.java
+++
@@ -1,7 +0,0 @@
-package org.interview.preperation.java.designpatterns.asyncmethodinvocation;
-
-import java.util.Optional;
-
-public interface AsyncCallback<T> {
-    void onComplete(T t, Optional<Exception> ex);
-}
